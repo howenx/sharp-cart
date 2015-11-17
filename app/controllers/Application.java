1084db39ce2cae23dab8c5ceef3aa705e0385381
howen <smart.hx@icloud.com>
2015-11-17 18:50:51 +0800

    modify cache

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,19 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import net.spy.memcached.MemcachedClient;
 import play.Logger;
+import play.cache.Cache;
+import play.cache.CacheApi;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 
-import java.io.IOException;
+import javax.inject.Inject;
 
 public class Application extends Controller {
 
+    @Inject
+    CacheApi cache;
 
     public Result index() throws Exception {
         return ok(views.html.index.render(Encryption.getInstance().getPublicKey(), Encryption.getInstance().getPrivateKey(), "123123", Encryption.handleEncrypt(Encryption.getInstance().getPrivateKey(), "123123"), Encryption.handleDecrypt(Encryption.getInstance().getPublicKey(),Encryption.handleEncrypt(Encryption.getInstance().getPrivateKey(), "123123"))));
@@ -22,12 +25,9 @@ public class Application extends Controller {
     }
 
     public Result getToken() {
-        try {
-            MemcachedClient cache_client = new MemcachedClient();
-            cache_client.set("123123",0,Encryption.handleEncrypt(Encryption.getInstance().getPrivateKey(), "123123"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+
+        cache.set("123123",Encryption.handleEncrypt(Encryption.getInstance().getPrivateKey(), "123123"));
+
         return ok(Encryption.handleEncrypt(Encryption.getInstance().getPrivateKey(), "123123"));
     }
 
