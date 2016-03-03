72b58f0eedd072f270761a4ea01595053d1fbbd1
howen <smart.hx@icloud.com>
2016-03-03 14:00:31 +0800

    sadf

--- app/controllers/MsgCtrl.java
+++ app/controllers/MsgCtrl.java
@@ -227,7 +227,7 @@ public class MsgCtrl extends Controller{
                     else
                         m.setMsgImg(SysParCom.IMAGE_URL + m.getMsgImg());
                     m.setMsgUrl(SysParCom.DEPLOY_URL+m.getMsgUrl());
-                    if (m.getMsgType().equals("V")) m.setMsgUrl(SysParCom.PROMOTION_URL+m.getMsgUrl());
+                    if (m.getTargetType().equals("V")) m.setMsgUrl(SysParCom.PROMOTION_URL+m.getMsgUrl());
                     if(m.getReadStatus()==1){
                         isHaveNotRead[0] =true;
                     }
