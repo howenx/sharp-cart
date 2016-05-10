46eb9168df52c43a8f89ca03a1502234b3278370
sibyl <sibyl.sun@kakaocorp.com>
2016-05-10 10:44:23 +0800

    dd

--- public/js/pay.js
+++ public/js/pay.js
@@ -2,7 +2,7 @@
 var appId = "";
 var timeStamp = "";
 var nonceStr = "";
-var pg = "";
+var package = "";
 var signType = "";
 var paySign = "";
 
@@ -82,6 +82,7 @@ function payUnifiedorder(tradeType,orderId,orderCreateAt,token,securityCode){
 
 function onBridgeReady(){
 console.log("appId="+appId+",timeStamp="+timeStamp+",nonceStr="+nonceStr+",package="+package+",signType="+signType+",paySign="+paySign)
+alert("appId="+appId+",timeStamp="+timeStamp+",nonceStr="+nonceStr+",package="+package+",signType="+signType+",paySign="+paySign)
    WeixinJSBridge.invoke(
        'getBrandWCPayRequest', {
               "appId":appId,     //公众号名称，由商户传入
@@ -90,15 +91,10 @@ console.log("appId="+appId+",timeStamp="+timeStamp+",nonceStr="+nonceStr+",packa
               "package":package,
               "signType":signType,         //微信签名方式：
               "paySign":paySign  //微信签名
-
-//           "appId" ： "wx2421b1c4370ec43b",     //公众号名称，由商户传入
-//           "timeStamp"：" 1395712654",         //时间戳，自1970年以来的秒数
-//           "nonceStr" ： "e61463f8efa94090b1f366cccfbbb444", //随机串
-//           "package" ： "prepay_id=u802345jgfjsdfgsdg888",
-//           "signType" ： "MD5",         //微信签名方式：
-//           "paySign" ： "70EA570631E4BB79628FBCA90534C63FF7FADD89" //微信签名
        },
        function(res){
+           console.log(res.err_msg);
+           alert(res.err_msg);
            if(res.err_msg == "get_brand_wcpay_request：ok" ) {}     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
        }
    );
