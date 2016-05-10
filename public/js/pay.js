d7083ac603914c66a8635ef08aa4482141635553
sibyl <sibyl.sun@kakaocorp.com>
2016-05-10 19:43:32 +0800

    dd

--- public/js/pay.js
+++ public/js/pay.js
@@ -121,8 +121,8 @@ function onBridgeReady(){
        },
        function(res){
            console.log(res.err_msg);
-           alert(res.err_msg);
-           if(res.err_msg == "get_brand_wcpay_request：ok" ) {// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+         //  alert(res.err_msg);
+           if(res.err_msg == "get_brand_wcpay_request:ok" ) {// 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
               //查询支付结果
               payOrderquery(orderId,token,securityCode);
            }else{
