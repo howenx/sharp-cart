2a7dff4ef0f6f903a9076e0392666c7c0fa6affd
sibyl.sun <sibyl.sun@kakaocorp.com>
2016-08-26 19:05:14 +0800

    dd

--- app/modules/LevelDBModule.java
+++ app/modules/LevelDBModule.java
@@ -19,8 +19,8 @@ public class LevelDBModule extends AbstractModule {
         bind(NewScheduler.class);
         bind(OrderMid.class);
         bind(SysParCom.class).asEagerSingleton();
-        bind(AppOnStart.class).asEagerSingleton();
-        bind(MnsInit.class).asEagerSingleton();
+//        bind(AppOnStart.class).asEagerSingleton();
+//        bind(MnsInit.class).asEagerSingleton();
         bind(LogUtil.class).asEagerSingleton();
         bind(RedisPool.class).asEagerSingleton();
         bind(LeveldbLoad.class).asEagerSingleton();
